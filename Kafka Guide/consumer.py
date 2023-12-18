016ff8d2b839c61d6f121aa67d30e6c846b1170d
Emilio Cimino <emilio.cimino@outlook.it>
2023-12-18 11:47:37 +0100

    Update consumer.py

--- Kafka Guide/consumer.py
+++ Kafka Guide/consumer.py
@@ -5,10 +5,23 @@ from json import loads
 from kafka import KafkaConsumer
 
 
+'''
+The following snippet of code teaches how to use a KafkaConsumer to consume data from a kafka topic. The following class encapsulate an instance of a KafkaConsumer and a method to deserialize message.
+It is possible to modify it using the following advices and/or any further hints from the provided library
+
+A KafkaConsumer is a client that consumes data from a kafka topic. 
+The base constructor of this client does not require any mandatory field, as explained in the docs: kafka.KafkaConsumer(*topics, **configs)
+It accepts an optional list of topics and a series of keyword arguments.
+
+From the topic perspective, it is possible to leave it empty and then subscribe to topics by using the "subscribe()" or "assign()" methods before receiving data
+About the keyword arguments, here a list of the most used and important ones and their meaning:
+
+'''
+
 class Consumer:
     def __init__(self):
         nums_list = []
-        self.consumer = KafkaConsumer('weather_data',
+        self.consumer = KafkaConsumer('my_topic',
                                       value_deserializer=lambda x: loads(x),
                                       group_id='my_weather_group',
                                       auto_offset_reset='latest')
