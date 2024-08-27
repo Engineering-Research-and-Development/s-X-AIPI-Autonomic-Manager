c206203ddd1534e53536cbfff9966144c149706c
Emilio Cimino <103200695+emiliocimino@users.noreply.github.com>
2024-08-27 11:34:50 +0200

    Adding solutions for asphalt use case Fixing merge threshold rules

--- src/dagster_service/commons/analysis_operations.py
+++ src/dagster_service/commons/analysis_operations.py
@@ -1,5 +1,5 @@
 from .utils import THRESHOLD_HIGH, THRESHOLD_LOW, THRESHOLD_OK, STATUS_GOOD, STATUS_BAD, HISTORY_BAD, \
-    HISTORY_GOOD, UNCONFIRMED, THRESHOLD_BROKEN
+    HISTORY_GOOD, UNCONFIRMED, THRESHOLD_BROKEN, THRESHOLD_ERROR
 from dagster import op
 
 
@@ -38,33 +38,59 @@ def discriminate_thresholds(lower_thresholds: list[float],
 
 
 @op
-def merge_thresholds_and(first_group: list[str],
-                         second_group: list[str],
-                         ) -> list[str]:
+def merge_thresholds(first_group: list[str],
+                     second_group: list[str],
+                     mode: str
+                     ) -> list[str]:
     """
-    Merge two groups of threshold results using logical AND operation.
+    Merge two groups of threshold results using logical operations.
+    This function takes results from the discriminate_thresholds function, meaning that basically it is necessary to
+    check against results from that function. Logical operations are performed on those. It is suggested to set
+    thresholds on values to be inclusive, then perform logical merging here.
+    For instance
+    x > 0 and y != K
+    means that, for discriminate_threshold function, THRESHOLD OK are on x > 0 and K<=y<=K
+
+    Modes:
+    X_AND_Y : Threshold on X is ok AND threshold on Y is OK -> overall OK
+    X_OR_Y: Thershold on X is ok OR threshold on Y is OK -> overall OK
+    X_AND_NOT_Y: Threshold on X is OK AND threshold on Y is NOT OK -> overall OK
+
 
     @param first_group: First group of threshold results.
     @param second_group: Second group of threshold results.
+    @param mode: decide if merge with AND / OR / XOR logical operations
 
     @return: Merged list of threshold results where THRESHOLD_OK is preserved only if both groups are THRESHOLD_OK,
              otherwise THRESHOLD_BROKEN is set.
     """
+    result_list = [THRESHOLD_ERROR]
 
-    result_list = []
     try:
         if len(first_group) != len(second_group):
             raise IndexError("Misconfiguration: list lengths are not equal")
 
         for first_result, second_result in zip(first_group, second_group):
-            if first_result == second_result and first_result == THRESHOLD_OK:
-                result_list.append(THRESHOLD_OK)
+            result = THRESHOLD_BROKEN
+            # This wants first threshold to be ok, second threshold to be broken (if broken is ok)
+            if mode == "X_AND_NOT_Y":
+                if (first_result == THRESHOLD_OK) and (second_result != THRESHOLD_OK):
+                    result = THRESHOLD_OK
+            elif mode == "X_OR_Y":
+                if first_result == THRESHOLD_OK or second_result == THRESHOLD_OK:
+                    result = THRESHOLD_OK
+            elif mode == "X_AND_Y":
+                if first_result == THRESHOLD_OK and second_result == THRESHOLD_OK:
+                    result = THRESHOLD_OK
             else:
-                result_list.append(THRESHOLD_BROKEN)
+                raise IndexError("Configured Mode does not Exist")
+
+            result_list = [result]
 
     except IndexError as e:
         # TODO: decide if insert error alarm or not
         print("An error occurred while merging thresholds:", e)
+        return result_list
 
     return result_list
 
